2747a314d4da0e967a75dfcfb0f5b7062a0cf0aa
Filipe Marques Hottis <lipehottis@gmail.com>
2023-03-28 16:33:48 -0300

    Continuação...

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/script-02.js
@@ -0,0 +1,29 @@
+// Tipos Primitivos
+let movie = 'Avengers'; // string literal
+let score = 10.87; // number literal
+let isVale = false; // boolean
+let name; // undefined
+let collor = null; // undefined - esperando redefinição 
+
+// Operradores Aritimédicos
+// +    ===     sum
+// -    ===     subtraction
+// *    ===     matiplication
+// /    ===     division
+// **   ===     exponentiantion
+
+// Tipagem Dinâmica
+// ++   ===     Adcionar + 1
+// --   ===     Descrementar 1
+// +=   ===     Adcionar o valor apos o =       MAIS RECOMENDADO
+// -=   ===     Valor de ante - valor apos =    MAIS RECOMENDADO
+
+let base = 5;
+let height = 8;
+let area = 5 * 8;
+let perimeter = (5*2) + (8*2);
+
+console.log('Base =', base)
+console.log('Altura =', height)
+console.log('Area =', area)
+console.log('Perímetro =', perimeter)
