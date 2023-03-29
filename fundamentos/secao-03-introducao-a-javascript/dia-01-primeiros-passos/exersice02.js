99c70b5058d6da084c6fa6a9c1038758a25037a9
Filipe Marques Hottis <lipehottis@gmail.com>
2023-03-29 14:06:46 -0300

    exercisios 1 ao 8 ok

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exersice02.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 3;
+
+if (a > b){
+    console.log('A é maior!');
+} else {
+    console.log('B é maior!')
+}
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exersice03.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 3;
+let c = 8;
+
+if (a > b && a > c){
+    console.log('A é maior!');
+} else if (b > a && b > c){
+    console.log('B é maior!');
+} else if (c > a && c > b){
+    console.log('C é maior!')
+} else {
+    console.log('Erro!!!')
+}
