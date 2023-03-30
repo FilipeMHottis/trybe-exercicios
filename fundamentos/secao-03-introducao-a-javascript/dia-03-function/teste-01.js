51e51c9e83e6c7a1b12f2efebc8261d4d249e791
Filipe Marques Hottis <lipehottis@gmail.com>
2023-03-30 15:10:48 -0300

    testes

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-function/teste-01.js
@@ -0,0 +1,19 @@
+function addSaldo(saldo, add){
+    let final = saldo + add;
+    return final
+}
+
+function subSaldo(saldo, sub){
+    let final = saldo - sub;
+    return final
+}
+
+function taxa(saldo, taxa){
+    let final = saldo * taxa;
+    return final
+}
+
+function divSaldo(saldo, div){
+    let final = saldo / div;
+    return final
+}
