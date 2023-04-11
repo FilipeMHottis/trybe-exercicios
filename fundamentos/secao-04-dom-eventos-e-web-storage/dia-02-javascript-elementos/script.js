7f178de8932e6b88698b93d84727e7a42f23be7d
Filipe Marques Hottis <lipehottis@gmail.com>
2023-04-11 18:57:14 -0300

    stage 2 and 4

--- fundamentos/secao-04-dom-eventos-e-web-storage/dia-02-javascript-elementos/script.js
+++ fundamentos/secao-04-dom-eventos-e-web-storage/dia-02-javascript-elementos/script.js
@@ -5,6 +5,7 @@ const h1 = document.createElement('h1');
 const h3 = document.createElement('h3');
 const p = document.createElement('p');
 const img = document.createElement('img');
+const body = document.getElementsByTagName('body')[0];
 
 // Function for text 
 function elemnetText (text, element, position) {
@@ -12,5 +13,14 @@ function elemnetText (text, element, position) {
     position.appendChild(element);
 }
 
-// 1 - Stage - ok
-elemnetText('TrybeTrip - Agência de Viagens', h1, document.getElementsByTagName('body')[0]);
+// Stage - 01
+elemnetText('TrybeTrip - Agência de Viagens', h1, body);
+h1.className = 'title';
+
+// Stage - 02
+body.appendChild(main).className = 'main-content';
+
+// Stage - 03
+document.querySelector('.main-content').appendChild(section).className = 'center-content';
+
+// Stage - 04
