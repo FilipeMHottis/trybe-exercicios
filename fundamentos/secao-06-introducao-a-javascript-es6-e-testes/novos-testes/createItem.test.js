57449bfcddb8b8ef1a1639af97725da9cc1c5f22
Filipe Marques Hottis <lipehottis@gmail.com>
2023-05-02 15:17:05 -0300

    comitão :D

--- fundamentos/secao-06-introducao-a-javascript-es6-e-testes/novos-testes/createItem.test.js
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes/novos-testes/createItem.test.js
@@ -7,9 +7,22 @@ describe('a função createItem', () => {
 
     expect(variavel).toEqual(result);
     });
-  it.todo('utiliza zero como quantidade padrão');
-  it.todo('Lança um erro quando não recebe parâmetros');
-  it.todo('Lança um erro se o nome do item não é uma string');
-  it.todo('Lança um erro se o preço é negativo');
-  it.todo('Lança um erro se o preço é zero');
+  it('utiliza zero como quantidade padrão', () => {
+    const variavel = createItem('banana', 'kg', 1.99);
+    const result = { name: 'banana', unit: 'kg', price: 1.99, quantity: 0 };
+
+    expect(variavel).toEqual(result);
+  });
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+  });
+  it('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(21, 'kg', 1.99)).toThrow('O nome do item deve ser uma string');
+  });
+  it('Lança um erro se o preço é negativo', () => {
+    expect(() => createItem('banana', 'kg', -1)).toThrow('O preço do item deve ser maior que zero');
+  });
+  it('Lança um erro se o preço é zero', () => {
+    expect(() => createItem('banana', 'kg', 0)).toThrow('O preço do item deve ser maior que zero');
+  });
 });
