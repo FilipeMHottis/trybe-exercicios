4b09cdac762940b03e21067d2a868a1d3b77dfe7
Filipe Marques Hottis <lipehottis@gmail.com>
2023-05-08 14:48:00 -0300

    inicio verdadeiro

---
+++ fundamentos/secao-07-HOF/test.js
@@ -0,0 +1,12 @@
+const newEmployees = () => {
+    const employees = {
+      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+};
+
+const geradoraDeEmail = (nome) => `${nome.toLowerCase().split(' ').join('_')}@trybe.com`;
+
+console.log(geradoraDeEmail('Filipe Marques Hottis'));
