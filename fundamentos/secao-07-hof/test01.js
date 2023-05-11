1955a686ed19c74dea1f016d0a4e4689e13995f3
Filipe Marques Hottis <lipehottis@gmail.com>
2023-05-11 14:46:03 -0300

    ok

---
+++ fundamentos/secao-07-hof/test01.js
@@ -0,0 +1,12 @@
+const primeNumbers = [17, 23, 37];
+
+const [ number1, number2, number3 ] = primeNumbers;
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2]); // 54
+sum(number1, number3);
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
