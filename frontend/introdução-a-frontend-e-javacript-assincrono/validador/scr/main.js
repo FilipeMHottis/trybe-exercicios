d6db34c66c02564e60f1d95ba11eafdd31f3f791
Filipe Marques Hottis <lipehottis@gmail.com>
2023-05-19 19:01:14 -0300

    tudo ok

--- frontend/introdução-a-frontend-e-javacript-assincrono/validador/scr/main.js
+++ frontend/introdução-a-frontend-e-javacript-assincrono/validador/scr/main.js
@@ -2,8 +2,14 @@ import validator from 'validator';
 import './style.css';
 
 const buttonSubmit = document.querySelector('#submit');
+const selectHTML = document.querySelector('select');
+const resposta = document.querySelector('h3');
+const inputText = document.querySelector('#inputText');
 
 buttonSubmit.addEventListener('click', () => {
-  console.log('Test');
-  console.log(validator.isEmail());
+  console.log(selectHTML.value);
+  if (selectHTML.value === 'email' && validator.isEmail(inputText.value)) {
+    resposta.innerHTML = 'Yes, Isso é um email.';
+  }
+  resposta.inert = 'No, Isso não é valido';
 });
