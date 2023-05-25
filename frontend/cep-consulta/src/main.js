237bb7c94c9ab3159facfa2a796a432704072e10
Filipe Marques Hottis <lipehottis@gmail.com>
2023-05-25 13:54:21 -0300

    inicio do projetinho, já fiz a primeira verificação

--- frontend/cep-consulta/src/main.js
+++ frontend/cep-consulta/src/main.js
@@ -1,11 +1,17 @@
 import './css/style.css';
 import './css/reset.style.css';
+import { inputVazio } from './utils/verificador';
 
 const buttonInput = document.querySelector('button');
 
 buttonInput.addEventListener('click', () => {
   const p = document.querySelector('section p span');
   const cpfInput = document.querySelector('main form input').value;
-  p.innerHTML = '';
-  p.innerHTML = cpfInput;
+
+  try {
+    inputVazio(cpfInput);
+    p.innerHTML = cpfInput;
+  } catch (error) {
+    p.innerHTML = error.message;
+  }
 });
